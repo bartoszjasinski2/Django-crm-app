243fbbc5eaaf6adf71e2c3fa48b53edfd936444c
Bartosz Jasinski <bartaspl5@gmail.com>
2023-08-03 13:21:07 +0200

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "You have been loggen in")
+			return redirect('home')
+		else:
+			messages.success(request, "There was an Error Logging in, please tgry again")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
+
+
+
+
+def logout_user(request):
+	print("000")
+	logout(request)
+	print("111")
+	messages.success(request, "You have beem Logged Out")
+	print("222")
+	return redirect('home')
+
+def register_user(request):
+	return render(request, 'register.html', {})
+
